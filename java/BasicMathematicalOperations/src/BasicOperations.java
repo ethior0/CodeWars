3bcf62675eeb64d47c9fe9f7ec849c8746317227
ethior0 <thiagomeigmoreira18@gmail.com>
2024-01-21 00:17:59 -0300

    Basic Mathematical Operations

---
+++ java/BasicMathematicalOperations/src/BasicOperations.java
@@ -0,0 +1,15 @@
+
+public class BasicOperations {
+
+	public static Integer basicMath(String op, int v1, int v2) {
+		
+		switch(op) {
+		case "+": return v1 + v2;
+		case "-": return v1 - v2;
+		case "*": return v1 * v2;
+		case "/": return v1 / v2;
+		}
+		
+		return 0;
+	}
+}
