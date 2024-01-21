ecf1c4b3928c1431211c121849066e923f77e7f2
ethior0 <thiagomeigmoreira18@gmail.com>
2024-01-21 00:24:07 -0300

    Beginner Series

---
+++ java/BG2Clock/src/Clock.java
@@ -0,0 +1,8 @@
+
+public class Clock {
+
+	public static int Past (int h, int m, int s) {
+		
+		return ((h * 3600) + (m * 60) + s) * 1000;
+	}
+}
---
+++ java/BGS4Cockroach/src/Cockroach.java
@@ -0,0 +1,8 @@
+
+public class Cockroach {
+
+	public static int cockroachSpeed(double x) {
+		
+		return (int) Math.floor( (x * 27.777777777777777777777777777778) );
+	}
+}
