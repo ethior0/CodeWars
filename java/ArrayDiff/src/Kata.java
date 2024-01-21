bcd4056d307b6f48ce2d5015e6cf271abf932cf7
ethior0 <thiagomeigmoreira18@gmail.com>
2024-01-20 23:41:59 -0300

    Array.diff(2)

--- java/ArrayDiff/src/Kata.java
+++ java/ArrayDiff/src/Kata.java
@@ -1,33 +1,33 @@
 
 public class Kata {
 
-	public static int[] arrayDiff(int[] a, int[] b) {
-
+  public static int[] arrayDiff(int[] a, int[] b) {
+		
 		int cc = 0;
-		int[] aux = new int[a.length];
-
-		for (int i = 0; i < a.length; i++) {
-			
+		int aux[] = new int[a.length];
+		
+		for ( int i = 0; i < a.length; i++ ) {
 			boolean found = false;
+			
 			for (int j = 0; j < b.length; j++) {
 				if (a[i] == b[j]) {
 					found = true;
 					break;
 				}
 			}
-
+			
 			if (!found) {
 				aux[cc] = a[i];
 				cc++;
 			}
 		}
-
-		int[] ans = new int[cc];
+		
+		int ans[] = new int[cc];
+		
 		for (int k = 0; k < cc; k++) {
 			ans[k] = aux[k];
 		}
-
+		
 		return ans;
-
-	}
+  }
 }
